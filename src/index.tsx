cb9eca293b5178ae65c7e371bb103b34d69e9660
ozgursoy <bugra.gocer@gmail.com>
2024-04-15 02:08:23 +0300

    new props

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { requireNativeComponent, ViewStyle } from "react-native";
+import { requireNativeComponent } from "react-native";
 
 const ComponentName = "StrokeTextView";
 
 type TextAlign = "center" | "left" | "right"
 
 export interface StrokeTextProps {
+  width?: number;
   text: string;
   fontSize?: number;
   color?: string;
   strokeColor?: string;
   strokeWidth?: number;
   fontFamily?: string;
-  style?: ViewStyle;
   align?: TextAlign;
+  numberOfLines?: number;
+  ellipsis?: boolean;
 }
 
 const NativeStrokeText = requireNativeComponent<StrokeTextProps>(ComponentName);
