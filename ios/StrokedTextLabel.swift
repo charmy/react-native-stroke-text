498e75339766d59b5899e78b34d7aefd2433425f
ozgursoy <bugra.gocer@gmail.com>
2024-04-14 06:17:56 +0300

    ios align padding fix

--- ios/StrokedTextLabel.swift
+++ ios/StrokedTextLabel.swift
@@ -29,6 +29,11 @@ class StrokedTextLabel: UILabel {
         let shadowOffset = self.shadowOffset
         let textColor = self.textColor
 
+        // Adjust the text drawing area to accommodate the outline width
+        let strokePadding = outlineWidth / 2
+        let insets = UIEdgeInsets(top: 0, left: strokePadding, bottom: 0, right: strokePadding)
+        let adjustedRect = rect.inset(by: insets)
+
         let c = UIGraphicsGetCurrentContext()
 
         c?.setLineWidth(outlineWidth)
@@ -37,18 +42,18 @@ class StrokedTextLabel: UILabel {
         self.textAlignment = align
         self.textColor = outlineColor
 
-        super.drawText(in: rect)
+        super.drawText(in: adjustedRect)
 
         if let shadowColor = shadowColor {
             super.shadowColor = shadowColor
             super.shadowOffset = shadowOffset
-            super.drawText(in: rect)
+            super.drawText(in: adjustedRect)
         }
 
         c?.setTextDrawingMode(.fill)
         self.textColor = textColor
         self.shadowOffset = CGSize(width: 0, height: 0)
-        super.drawText(in: rect)
+        super.drawText(in: adjustedRect)
 
         self.shadowOffset = shadowOffset
     }
